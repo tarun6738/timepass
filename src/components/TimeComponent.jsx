81e1cfc88f103e68854f6143a819c2c59acd8254
tarun6738 <tarunnemali2004@gmail.com>
2024-04-17 21:41:57 +0530

    another

--- src/components/TimeComponent.jsx
+++ src/components/TimeComponent.jsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 const TimeComponent = ({ id, onClose, uniqueListPlace, selectedItem }) => {
   const [selectedHour, setSelectedHour] = useState(0);
   const [showTimeList, setShowTimeList] = useState(false);
-  const [uniqueListPlaceLocal, setUniqueListPlaceLocal] = useState(uniqueListPlace);
+  const [uniqueListPlaceLocal, setUniqueListPlaceLocal] =
+    useState(uniqueListPlace);
   const [selectedItemLocal, setSelectedItemLocal] = useState(selectedItem);
-  
 
   const handleChange = (event) => {
     setSelectedHour(parseInt(event.target.value));
@@ -20,12 +20,13 @@ const TimeComponent = ({ id, onClose, uniqueListPlace, selectedItem }) => {
     setSelectedItemLocal(selectedItem);
   }, [selectedItem]);
 
-
   const generateTimeOptions = () => {
     const options = [];
     for (let hour = 0; hour < 24; hour++) {
       for (let minute = 0; minute < 60; minute += 30) {
-        const time = `${formatHour(hour)}:${formatMinute(minute)} ${hour < 12 ? 'am' : 'pm'}`;
+        const time = `${formatHour(hour)}:${formatMinute(minute)} ${
+          hour < 12 ? "am" : "pm"
+        }`;
         options.push(
           <div
             key={time}
@@ -41,12 +42,12 @@ const TimeComponent = ({ id, onClose, uniqueListPlace, selectedItem }) => {
   };
 
   const formatHour = (hour) => {
-    if (hour === 0) return '12'; // Midnight
+    if (hour === 0) return "12"; // Midnight
     if (hour > 12) return `${hour - 12}`; // After noon
     return `${hour}`; // Before noon
   };
 
-  const formatMinute = (minute) => { 
+  const formatMinute = (minute) => {
     return minute < 10 ? `0${minute}` : `${minute}`;
   };
 
@@ -66,13 +67,22 @@ const TimeComponent = ({ id, onClose, uniqueListPlace, selectedItem }) => {
   const formattedTime = () => {
     const hour = Math.floor(selectedHour / 60);
     const minute = selectedHour % 60;
-    const period = hour < 12 ? 'am' : 'pm';
+    const period = hour < 12 ? "am" : "pm";
     return `${formatHour(hour)}:${formatMinute(minute)} ${period}`;
   };
 
   const maxSliderValue = 23 * 60 + 45; // Maximum value representing 11:45 PM
 
-  const sliderLabels = ['12 AM', '3 AM', '6 AM', '9 AM', '12 PM', '3 PM', '6 PM', '9 PM'];
+  const sliderLabels = [
+    "12 AM",
+    "3 AM",
+    "6 AM",
+    "9 AM",
+    "12 PM",
+    "3 PM",
+    "6 PM",
+    "9 PM",
+  ];
 
   // Calculate slider positions for labels
   const sliderPositions = sliderLabels.map((label, index) => {
@@ -80,76 +90,133 @@ const TimeComponent = ({ id, onClose, uniqueListPlace, selectedItem }) => {
   });
 
   console.log("uniqueListPlace:", uniqueListPlaceLocal);
-  console.log("selectedItem:",selectedItemLocal);
+  console.log("selectedItem:", selectedItemLocal);
 
-  
- 
+  const DateandTime = selectedItemLocal;
 
-// Keep the time part unchanged: "20:16"
+  const parts = DateandTime.split(" ");
 
+  const place = parts[0];
 
-  
+  console.log(place);
 
-  
+  const datePart = parts[1]; // Extract the date part
+  const timePart = parts[2]; // Extract the time part
+  const gmt = parts[5];
 
+  // Convert the date part to the desired format: "Sun, Apr 17"
+  const date = new Date(datePart);
+  const day = date.toLocaleString("en-US", { weekday: "short" });
+  const month = date.toLocaleString("en-US", { month: "short" });
+  const formattedDate = `${day}, ${month} ${date.getDate()}`;
 
+  const formatTime = timePart.slice(0, 5); // Extract the first 5 characters
+
+  console.log("Formatted Date:", formattedDate);
+  console.log("Formatted Time:", formatTime);
 
   return (
     <div>
-    <div className='lg:w-4/5 mx-auto mt-2 border border-blue-300 p-4 h-54 relative'>
-      <button onClick={() => onClose(id)} className="absolute top-0 right-0 m-2 text-xl">&times;</button>
-      <div className='flex justify-between'>
-        {/* Timezone and Time */}
-        <div><h2 className='text-4xl font-bold'>{uniqueListPlace && uniqueListPlace.abbreviation}</h2></div>
-        <div className='flex w-64 lg:w-2/5 md:w-2/5 sm:w-2/5'>
-          <input 
-            type='text' 
-            value={formattedTime()}
-            className='text-4xl font-bold border border-black-400 shadow-sm w-4/6 text-center p-1'
+      <div className="lg:w-4/5 mx-auto mt-2 border border-blue-300 p-4 h-54 relative">
+        <button
+          onClick={() => onClose(id)}
+          className="absolute top-0 right-0 m-2 text-xl"
+        >
+          &times;
+        </button>
+        <div className="flex justify-between">
+          {/* Timezone and Time */}
+          <div>
+            <h2 className="text-4xl font-bold">
+              {uniqueListPlace && uniqueListPlace.abbreviation}
+              {uniqueListPlace.name && `${uniqueListPlace.name}`}
+            </h2>
+          </div>
+          <div className="flex w-64 lg:w-2/5 md:w-2/5 sm:w-2/5">
+            <input
+              type="text"
+              value={selectedHour === 0 ? formatTime : formattedTime()}
+              className="text-4xl font-bold border border-black-400 shadow-sm w-4/6 text-center p-1"
+              onChange={handleChange}
+              onClick={handleInputClick}
+              readOnly
+            />
+            {showTimeList && (
+              <div
+                className="absolute mt-10 w-40 max-h-36 overflow-y-auto bg-white border border-gray-300 rounded-md shadow-lg"
+                style={{ zIndex: 999 }}
+              >
+                {generateTimeOptions()}
+              </div>
+            )}
+          </div>
+        </div>
+
+        <div className="flex justify-between mt-2">
+          {/* Full form, GMT, Current Date */}
+          <div>
+            <p>
+              {uniqueListPlaceLocal.fullName
+                ? uniqueListPlaceLocal.fullName
+                : place && `${place}`}
+            </p>
+          </div>
+
+          <div className="flex w-64 lg:w-2/5 md:w-2/5 sm:w-2/5">
+            <p className="w-1/2">
+              GMT{" "}
+              <span>
+                {uniqueListPlaceLocal.gmtOffset
+                  ? uniqueListPlaceLocal.gmtOffset
+                  : gmt && `${gmt}`}
+              </span>
+            </p>
+            <p className="w-1/2">{formattedDate}</p>
+          </div>
+        </div>
+
+        <div className="items-center mt-4 relative">
+          <input
+            type="range"
+            min="0"
+            max={maxSliderValue}
+            step="15"
+            value={selectedHour}
             onChange={handleChange}
-            onClick={handleInputClick}
-            readOnly 
+            className="w-full appearance-none bg-gray-200 h-4 rounded-full"
+            style={{ zIndex: 0 }}
           />
-          {showTimeList && (
-            <div className="absolute mt-10 w-40 max-h-36 overflow-y-auto bg-white border border-gray-300 rounded-md shadow-lg" style={{ zIndex: 999 }}>
-              {generateTimeOptions()}
-            </div>
-          )}
+          <div className="w-full flex justify-between text-sm mt-2">
+            {sliderLabels.map((label, index) => (
+              <span
+                key={index}
+                className="text-center ml-2"
+                style={{
+                  flex: "1 0 auto",
+                  position: "absolute",
+                  left: `${(sliderPositions[index] / maxSliderValue) * 100}%`,
+                  transform: "translateX(-50%)",
+                }}
+              >
+                {label}
+              </span>
+            ))}
+          </div>
+          <div className="h-4 mt-2 flex justify-between">
+            {sliderLabels.map((label, index) => (
+              <div
+                key={index}
+                className="w-1 bg-gray-400"
+                style={{
+                  position: "absolute",
+                  left: `${(sliderPositions[index] / maxSliderValue) * 100}%`,
+                  transform: "translateX(-50%)",
+                }}
+              ></div>
+            ))}
+          </div>
         </div>
       </div>
-
-      <div className='flex justify-between mt-2'>
-        {/* Full form, GMT, Current Date */}
-        <div><p>{uniqueListPlaceLocal.fullName}</p></div>
-        <div className="flex w-64 lg:w-2/5 md:w-2/5 sm:w-2/5">
-          <p className="w-1/2">GMT <span>{uniqueListPlaceLocal.gmtOffset}</span></p>
-          <p className="w-1/2">Hello</p>
-        </div>
-      </div>
-
-      <div className="items-center mt-4 relative">
-        <input
-          type="range"
-          min="0"
-          max={maxSliderValue}
-          step="15"
-          value={selectedHour}
-          onChange={handleChange}
-          className="w-full appearance-none bg-gray-200 h-4 rounded-full"
-          style={{ zIndex: 0 }}
-        />
-        <div className="w-full flex justify-between text-sm mt-2">
-          {sliderLabels.map((label, index) => (
-            <span key={index} className="text-center ml-2" style={{ flex: '1 0 auto', position: 'absolute', left: `${(sliderPositions[index] / maxSliderValue) * 100}%`, transform: 'translateX(-50%)' }}>{label}</span>
-          ))}
-        </div>
-        <div className="h-4 mt-2 flex justify-between">
-          {sliderLabels.map((label, index) => (
-            <div key={index} className="w-1 bg-gray-400" style={{ position: 'absolute', left: `${(sliderPositions[index] / maxSliderValue) * 100}%`, transform: 'translateX(-50%)' }}></div>
-          ))}
-        </div>
-      </div>
-    </div>
     </div>
   );
 };
